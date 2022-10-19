c0ebd2dbbc9bf8ccb98a97206b28eb7c1891d887
Jhonata Melo <josejhonatanxd@gmail.com>
2022-10-19 10:47:19 -0500

    Ya se crean las imagenes por cada pokemon

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
 window.addEventListener('DOMContentLoaded',consumoApi)
 
+const containerCards = document.querySelector('.containerCards')
+
 function consumoApi(){
     fetch('https://pokeapi.co/api/v2/pokemon/')
     .then(response=>response.json())
     .then(response=>response.results.map(element=>{
-        createCards(element)
+        consumirPokemon(element)
     }))
 }
 
-function consumirPokemon(newUrl){
+function consumirPokemon(element){
+
+    let newUrl = element.url
+
     fetch(newUrl)
     .then(response=>response.json())
     .then(data=>{
         console.log(data);
+        createCards(data)
     })
 }
 
-const containerCards = document.querySelector('.containerCards')
+function createCards(data){
 
-function createCards(pokemon){
-
-const {name,url} = pokemon
+    console.log(data);
 
 const card = document.createElement('div')
-card.setAttribute('alt',name)
 card.classList.add('card')
 
+const imagePokemon =document.createElement('img')
+imagePokemon.setAttribute('src',data.sprites.front_default)
 
 const namePokemon = document.createElement('h1')
-namePokemon.textContent = name
+namePokemon.textContent = data.name
 
 containerCards.appendChild(card)
 
 card.appendChild(namePokemon)
-
-consumirPokemon(url)
+card.appendChild(imagePokemon)
 
 }
 
