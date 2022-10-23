abf5bb5e8b7839fde3bdc839848807adcd9c261f
Jhonata Melo <josejhonatanxd@gmail.com>
2022-10-23 16:00:09 -0500

    Ya esta la info de cada pokemon en las cards, y el buscador funciona. Falta estilizar

--- app.js
+++ app.js
@@ -26,70 +26,106 @@ function consumirPokemon(element){
     })
 }
 
-const typeColors = {
-    electric: '#FFEA70',
-    normal: '#B09398',
-    fire: '#FF675C',
-    water: '#0596C7',
-    ice: '#AFEAFD',
-    rock: '#999799',
-    flying: '#7AE7C7',
-    grass: '#4A9681',
-    psychic: '#FFC6D9',
-    ghost: '#561D25',
-    bug: '#A2FAA3',
-    poison: '#795663',
-    ground: '#D2B074',
-    dragon: '#DA627D',
-    steel: '#1D8A99',
-    fighting: '#2F2F2F',
-    default: '#2A1A1F',
-}
-
 // Renderizar pokemones
 
 function createCards(data){
 
-// console.log(data);
+    console.log(data);
 
-const card= document.createElement('div')
-card.classList.add('card')
+    const card= document.createElement('div')
+    card.classList.add('card')
 
-const rotomDex = document.createElement('div')
-rotomDex.classList.add('rotomDex')
+    const rotomDex = document.createElement('div')
+    rotomDex.classList.add('rotomDex')
 
-const firstView = document.createElement('div')
-firstView.classList.add('firstView')
+    const firstView = document.createElement('div')
+    firstView.classList.add('firstView')
 
-const containerImg = document.createElement('div')
-containerImg.classList.add('containerImg')
+    const containerImg = document.createElement('div')
+    containerImg.classList.add('containerImg')
 
-const imagePokemon =document.createElement('img')
-imagePokemon.setAttribute('src',data.sprites.front_default)
-imagePokemon.classList.add('img')
+    const imagePokemon =document.createElement('img')
+    imagePokemon.setAttribute('src',data.sprites.front_default)
+    imagePokemon.classList.add('img')
 
-const namePokemon = document.createElement('h1')
-namePokemon.textContent = data.name
+    const namePokemon = document.createElement('h1')
+    namePokemon.textContent = data.name
 
-const idPokemon = document.createElement('h3')
-idPokemon.textContent = "N° "+ data.id
+    const idPokemon = document.createElement('h3')
+    idPokemon.textContent = "N° "+ data.id
 
-const features = document.createElement('div')
-features.classList.add('features')
+    const features = document.createElement('div')
+    features.classList.add('features')
 
-containerCards.appendChild(card)
+    const typePokemon = document.createElement('div')
+    typePokemon.classList.add('divStat')
+    const typeP = document.createElement('p')
+    typeP.textContent = "Type: "
+    const typeP2 = document.createElement('p')
+    typeP2.textContent = data.types[0].type.name
 
-card.appendChild(rotomDex)
-card.appendChild(firstView)
-firstView.appendChild(namePokemon)
-firstView.appendChild(containerImg)
-containerImg.appendChild(imagePokemon)
-firstView.appendChild(idPokemon)
+    const statPokemon1 = document.createElement('div')
+    statPokemon1.classList.add('divStat')
+    const hability = document.createElement('p')
+    hability.textContent = "Hability: "
+    const hability2 = document.createElement('p')
+    hability2.textContent = data.abilities[1].ability.name
 
-card.appendChild(features)
+    const statPokemon2 = document.createElement('div')
+    statPokemon2.classList.add('divStat')
+    const hp = document.createElement('p')
+    hp.textContent = data.stats[0].stat.name
+    const hp2 = document.createElement('p')
+    hp2.textContent = data.stats[0].base_stat
+
+    const statPokemon3 = document.createElement('div')
+    statPokemon3.classList.add('divStat')
+    const attack = document.createElement('p')
+    attack.textContent = data.stats[1].stat.name
+    const attack2 = document.createElement('p')
+    attack2.textContent = data.stats[1].base_stat
+
+    const statPokemon4 = document.createElement('div')
+    statPokemon4.classList.add('divStat')
+    const defense = document.createElement('p')
+    defense.textContent = data.stats[2].stat.name
+    const defense2 = document.createElement('p')
+    defense2.textContent = data.stats[2].base_stat
+
+    containerCards.appendChild(card)
+
+    card.appendChild(rotomDex)
+    card.appendChild(firstView)
+    firstView.appendChild(namePokemon)
+    firstView.appendChild(containerImg)
+    containerImg.appendChild(imagePokemon)
+    firstView.appendChild(idPokemon)
+
+    card.appendChild(features)
+
+    features.appendChild(typePokemon)
+    typePokemon.appendChild(typeP)
+    typePokemon.appendChild(typeP2)
+
+    features.appendChild(statPokemon1)
+    statPokemon1.appendChild(hability)
+    statPokemon1.appendChild(hability2)
+
+    features.appendChild(statPokemon2)
+    statPokemon2.appendChild(hp)
+    statPokemon2.appendChild(hp2)
+
+    features.appendChild(statPokemon3)
+    statPokemon3.appendChild(attack)
+    statPokemon3.appendChild(attack2)
+
+    features.appendChild(statPokemon4)
+    statPokemon4.appendChild(defense)
+    statPokemon4.appendChild(defense2)
 
 }
 
+// Pokedex Buscador
 
 const search = document.getElementById('search')
 
@@ -107,33 +143,85 @@ function searchPokemon(event){
         Pokedex(data)
     }).catch(err=>err+alert(error))
     }
-     
 }
 
-const containerPoke = document.getElementById('containerPoke')
+const midMain = document.getElementById('mid-main')
+const pokeinfo = document.getElementById('pokeinfo')
 
 function Pokedex(data){
 
-    containerPoke.innerHTML = ''
+    midMain.innerHTML = ''
+    pokeinfo.innerHTML = ''
 
     const Containerpokemon = document.createElement('div')
-    Containerpokemon.classList.add('containerImg')
+    Containerpokemon.classList.add('containerImgPoke')
 
     const pokemon = document.createElement('img')
     pokemon.setAttribute('src',data.sprites.front_default)
-    pokemon.classList.add('img')
+    pokemon.classList.add('imgPoke')
 
     const infoPokemon = document.createElement('div')
     infoPokemon.classList.add('infoPokemon')
 
-    const namePokemon2 = document.createElement('h1')
-    namePokemon2.textContent = data.name
-    
+    const typePokemon = document.createElement('div')
+    typePokemon.classList.add('divStat')
+    const typeP = document.createElement('p')
+    typeP.textContent = "Type: "
+    const typeP2 = document.createElement('p')
+    typeP2.textContent = data.types[0].type.name
 
-    containerPoke.appendChild(Containerpokemon)
+    const statPokemon1 = document.createElement('div')
+    statPokemon1.classList.add('divStat')
+    const hability = document.createElement('p')
+    hability.textContent = "Hability: "
+    const hability2 = document.createElement('p')
+    hability2.textContent = data.abilities[1].ability.name
+
+    const statPokemon2 = document.createElement('div')
+    statPokemon2.classList.add('divStat')
+    const hp = document.createElement('p')
+    hp.textContent = data.stats[0].stat.name
+    const hp2 = document.createElement('p')
+    hp2.textContent = data.stats[0].base_stat
+
+    const statPokemon3 = document.createElement('div')
+    statPokemon3.classList.add('divStat')
+    const attack = document.createElement('p')
+    attack.textContent = data.stats[1].stat.name
+    const attack2 = document.createElement('p')
+    attack2.textContent = data.stats[1].base_stat
+
+    const statPokemon4 = document.createElement('div')
+    statPokemon4.classList.add('divStat')
+    const defense = document.createElement('p')
+    defense.textContent = data.stats[2].stat.name
+    const defense2 = document.createElement('p')
+    defense2.textContent = data.stats[2].base_stat
+        
+
+    midMain.appendChild(Containerpokemon) //imagen
     Containerpokemon.appendChild(pokemon)
 
-    containerPoke.appendChild(infoPokemon)
-    infoPokemon.appendChild(namePokemon2)
+    pokeinfo.appendChild(infoPokemon) //info
+
+    infoPokemon.appendChild(typePokemon)
+    typePokemon.appendChild(typeP)
+    typePokemon.appendChild(typeP2)
+
+    infoPokemon.appendChild(statPokemon1)
+    statPokemon1.appendChild(hability)
+    statPokemon1.appendChild(hability2)
+
+    infoPokemon.appendChild(statPokemon2)
+    statPokemon2.appendChild(hp)
+    statPokemon2.appendChild(hp2)
+
+    infoPokemon.appendChild(statPokemon3)
+    statPokemon3.appendChild(attack)
+    statPokemon3.appendChild(attack2)
+
+    infoPokemon.appendChild(statPokemon4)
+    statPokemon4.appendChild(defense)
+    statPokemon4.appendChild(defense2)
 }
 
