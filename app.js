7df9f9a774b4b9e4f3110b002c47bb9a5aa90395
Jhonata Melo <josejhonatanxd@gmail.com>
2022-10-23 12:00:33 -0500

    Buscador Agregado, falta traer la info del pokemon en las cards y buscador + estilizarlos

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ window.addEventListener('DOMContentLoaded',consumoApi)
 
 const containerCards = document.querySelector('.containerCards')
 
+// Consumir pokemones
+
 function consumoApi(){
     fetch('https://pokeapi.co/api/v2/pokemon/')
     .then(response=>response.json())
@@ -10,6 +12,8 @@ function consumoApi(){
     }))
 }
 
+// Consumir las caracteristicas de los pokemones
+
 function consumirPokemon(element){
 
     let newUrl = element.url
@@ -17,27 +21,48 @@ function consumirPokemon(element){
     fetch(newUrl)
     .then(response=>response.json())
     .then(data=>{
-        console.log(data);
+        // console.log(data);
         createCards(data)
     })
 }
 
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+}
+
+// Renderizar pokemones
+
 function createCards(data){
 
-console.log(data);
+// console.log(data);
 
-const principal = document.createElement('div')
-principal.classList.add('principal')
-
-const card = document.createElement('div')
+const card= document.createElement('div')
 card.classList.add('card')
 
-const containerImg = document.createElement('div')
-containerImg.classList.add('block')
+const rotomDex = document.createElement('div')
+rotomDex.classList.add('rotomDex')
 
-const CircleContainer = document.createElement('div')
-CircleContainer.classList.add('CircleImg')
-CircleContainer.classList.add('effect')
+const firstView = document.createElement('div')
+firstView.classList.add('firstView')
+
+const containerImg = document.createElement('div')
+containerImg.classList.add('containerImg')
 
 const imagePokemon =document.createElement('img')
 imagePokemon.setAttribute('src',data.sprites.front_default)
@@ -47,21 +72,68 @@ const namePokemon = document.createElement('h1')
 namePokemon.textContent = data.name
 
 const idPokemon = document.createElement('h3')
-idPokemon.textContent = "N° "+data.id
+idPokemon.textContent = "N° "+ data.id
 
 const features = document.createElement('div')
 features.classList.add('features')
 
 containerCards.appendChild(card)
 
-card.appendChild(namePokemon)
-card.appendChild(containerImg)
-card.appendChild(idPokemon)
-
-containerImg.appendChild(CircleContainer)
-CircleContainer.appendChild(imagePokemon)
+card.appendChild(rotomDex)
+card.appendChild(firstView)
+firstView.appendChild(namePokemon)
+firstView.appendChild(containerImg)
+containerImg.appendChild(imagePokemon)
+firstView.appendChild(idPokemon)
 
 card.appendChild(features)
 
+}
+
+
+const search = document.getElementById('search')
+
+search.addEventListener('keyup',searchPokemon)
+
+let error = "No se encontró el Pokemon"
+
+function searchPokemon(event){
+
+    if(event.code === 'Enter'){
+        fetch(`https://pokeapi.co/api/v2/pokemon/${search.value}/`)
+        .then(response=>response.json())
+        .then(data=>{
+        console.log(data);
+        Pokedex(data)
+    }).catch(err=>err+alert(error))
+    }
+     
+}
+
+const containerPoke = document.getElementById('containerPoke')
+
+function Pokedex(data){
+
+    containerPoke.innerHTML = ''
+
+    const Containerpokemon = document.createElement('div')
+    Containerpokemon.classList.add('containerImg')
+
+    const pokemon = document.createElement('img')
+    pokemon.setAttribute('src',data.sprites.front_default)
+    pokemon.classList.add('img')
+
+    const infoPokemon = document.createElement('div')
+    infoPokemon.classList.add('infoPokemon')
+
+    const namePokemon2 = document.createElement('h1')
+    namePokemon2.textContent = data.name
+    
+
+    containerPoke.appendChild(Containerpokemon)
+    Containerpokemon.appendChild(pokemon)
+
+    containerPoke.appendChild(infoPokemon)
+    infoPokemon.appendChild(namePokemon2)
 }
 
